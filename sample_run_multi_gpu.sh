d847ab76785fc4662e03911485378f4dd9f4a5a2
hankcs <jfservice@126.com>
2019-05-02 11:22:08 -0400

    run script

---
+++ sample_run_multi_gpu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+python3 run_pretraining.py \
+  --input_file=sample.tfrecord \
+  --output_dir=sample_wiki_uncased_L-12_H-768_A-12 \
+  --do_train=True \
+  --do_eval=True \
+  --bert_config_file=bert_config.json \
+  --train_batch_size=32 \
+  --max_seq_length=128 \
+  --max_predictions_per_seq=20 \
+  --num_train_steps=1000 \
+  --num_warmup_steps=10 \
+  --learning_rate=2e-5

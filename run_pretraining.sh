b7e0ca578a0ea6c1954782c59987bfc3850b979f
vincentcheny <cy0906@163.com>
2020-02-08 22:00:52 +0800

    simplify running procedure

--- run_pretraining.sh
+++ run_pretraining.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-python run_pretraining.py\
- --task_index=$1
- --input_file=./tf_examples.tfrecord\ 
- --output_dir=./pretraining_output \
- --do_train=True \
- --do_eval=True \
- --bert_config_file=./bert_config.json \
- --num_train_steps=20 \
- --num_warmup_steps=10 \
- --learning_rate=2e-5 \
- --multi_worker=True
+python run_pretraining.py --task_index=$1 --input_file=./tf_examples.tfrecord --output_dir=./estimator --do_train=True --do_eval=True --bert_config_file=./bert_config.json --num_train_steps=20 --num_warmup_steps=10 --learning_rate=2e-5 --multi_worker=True
